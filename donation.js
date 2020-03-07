838067e4b415827bfe702422b7e5b23f70f6efea
Hyprlynk <31111057+Hyprlynk@users.noreply.github.com>
2020-03-07 17:11:08 -0500

    Delete donation.js

--- donation.js
+++
@@ -1,9 +0,0 @@
- var totalDonationsNode = document.getElementById("totalDonations")
-    var totalMoney = 0;
-
-    function addMoney() {
-      totalMoney = parseInt(document.getElementById("donationAmount").value, 10) + totalMoney;
-      totalDonationsNode.textContent = totalMoney;
-    }
-
-    document.getElementById("donateButton").addEventListener("click", addMoney);
